eabafa6ec9cb17296dd9d87faf65b6a3af096723
RomanMillan <romandos962m@gmail.com>
2022-11-06 17:56:30 +0100

    errores varios

--- cochesMillan/src/main/java/com/jacaranda/UserCreate.java
+++ cochesMillan/src/main/java/com/jacaranda/UserCreate.java
@@ -1,6 +1,8 @@
 package com.jacaranda;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,32 +10,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Main
+ * Servlet implementation class UserCreate
  */
-@WebServlet("/Main")
-public class Main extends HttpServlet {
+@WebServlet("/UserCreate")
+public class UserCreate extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * Default constructor. 
      */
-    public Main() {
-        // TODO Auto-generated constructor stub
+    public UserCreate() {
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter salida = response.getWriter();
+		salida.println("<!DOCTYPE html>\r\n"
+				+ "<html lang=\"en\">\r\n"
+				+ "<head>\r\n"
+				+ "    <meta charset=\"UTF-8\">\r\n"
+				+ "    <title>Prueba</title>\r\n"
+				+ "</head>\r\n"
+				+ "<body>\r\n"
+				+ "    <h1>Esto es una prueba</h1>\r\n"
+				+ "</body>\r\n"
+				+ "</html>");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
